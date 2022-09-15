5235328abc0966b7a3a9afe14a10aac883f39901
lhjwork <jin.come.up.business@gmail.com>
2022-09-15 12:44:50 +0900

    뷰 추가, 뷰가 실제로 뭔가를 하도록 만들기

--- README.md
+++ README.md
@@ -3,3 +3,25 @@ polls(app에 해당함)
 
 python manage.py migrate
 python manage.py makemigrations polls
+
+관리자 id : admin
+관리자 password : 1234
+
+## -----------------------------------------------------------------------
+
+Polls/admin.py 에서 아래 코드를 입력해서 admin 에서 데이터를 직접 관리해보는 것 까지 해봄
+
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
+
+## -----------------------------------------------------------------------
+
+작업 진행
+뷰 추가하기
+뷰가 실제로 뭔가를 하도록 만들기
+지름길 : render()
+404에러 일으키기
+지름길: get_object_or_404()
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,46 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+from django.shortcuts import get_object_or_404,render
+from django.http import Http404
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# 장고 튜토리얼 코드 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# 강의용 코드 404가 아닌 에러가 잡힌다.
+# def detail(request, question_id):
+#     q = Question.objects.get(pk=question_id)
+#     context = {'question':q}
+#     return render(request, 'polls/detail.html', context)
+
+# 지름길: get_object_or_404()
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
